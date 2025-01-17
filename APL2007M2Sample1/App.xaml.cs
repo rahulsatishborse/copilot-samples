e59fd181ba5e45286dcd54e8ca21ba485d0b84e2
Rahul Borse <rahul.borse09@gmail.com>
2025-01-17 09:50:34 +0000

    Refactor App.xaml.cs file

--- APL2007M2Sample1/App.xaml.cs
+++ APL2007M2Sample1/App.xaml.cs
@@ -1,7 +1,17 @@
 ﻿using System.Windows;
 
 namespace ParallelAsyncExample
+{using System.Windows;
+
+namespace ParallelAsyncExample
 {
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
     public partial class App : Application
     {
     }
